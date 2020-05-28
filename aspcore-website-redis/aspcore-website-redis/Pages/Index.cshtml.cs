58d9ff3df34111cda7e8d488a33dcf2f2c0041a2
shashank <shashank.vittal87@gmail.com>
2020-05-28 16:14:02 +1000

    Added a very simple asp.net core web apps

---
+++ aspcore-website-redis/aspcore-website-redis/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServiceStack.Redis;
+
+namespace aspcore_website_redis.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public int Counter { get; set; } = 0;
+
+        public void OnGet()
+        {
+            var manager = new RedisManagerPool("redis-server-docker");
+            using (var client = manager.GetClient())
+            {
+                try
+                {
+                    Counter = client.Get<int>("CounterKey");
+                }
+                catch(Exception e)
+                {
+                    client.Set("CounterKey", Counter);
+                }
+                Counter = Counter + 1;
+                client.Set("CounterKey", Counter);
+               
+            }
+        }
+    }
+}
